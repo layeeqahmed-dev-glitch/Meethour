e0833e86380adf1947ba870f2086ea5c12bd98e9
layeeqahmed-dev-glitch <layeeqahmed.dev@gmail.com>
2026-05-01 19:47:47 +0530

    fixed db.js code

--- db.js
+++ db.js
@@ -13,9 +13,13 @@ const connectDB = async () => {
   }
 
   try {
-    console.log("🔍 USING URI:", process.env.MONGO_URI); // 👈 ADD THIS
+    console.log("🔍 USING URI:", process.env.MONGO_URI);
 
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000,          // 45 seconds
+      connectTimeoutMS: 30000,         // 30 seconds
+    });
 
     isConnected = db.connections[0].readyState;
     console.log('MongoDB Connected! ✅');
