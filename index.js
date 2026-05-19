21b6351f7a7c98c37a1f81fe8c69619810683c1e
layeeqahmed-dev-glitch <layeeqahmed.dev@gmail.com>
2026-05-19 20:50:49 +0530

    fixed: format meeting details

--- index.js
+++ index.js
@@ -709,13 +709,14 @@ app.post('/deal-webhook', async (req, res) => {
       console.log('Meeting saved to DB!');
 
       const formattedTime = `${meeting_time} ${meeting_meridiem} (${timezone})`;
-      const noteBody =
-        `${ownerName} is inviting you to a scheduled meeting.\n` +
-        `Topic: ${meeting.topic}\n` +
-        `Time: ${formattedTime}\n` +
-        `Join MeetHour Meeting: ${meeting.joinURL}\n` +
-        `Meeting ID: ${meeting.meeting_id}\n` +
-        `Passcode: ${meeting.passcode}`;
+      const noteBody = `
+        <b>${ownerName} is inviting you to a scheduled meeting.</b><br>
+        <b>Topic:</b> ${meeting.topic}</br>
+        <b>Time:</b> ${formattedTime}</br>
+        <b>Join MeetHour Meeting</b>: ${meeting.joinURL}<br>
+        <b>Meeting ID:</b> ${meeting.meeting_id}</br>
+        <b>Passcode:</b> ${meeting.passcode}</br>
+      `;
 
       await axios.post(
         'https://api.hubapi.com/crm/v3/objects/notes',
