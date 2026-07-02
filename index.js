8425d253908e4cf82a0690bf20dd38abbfe5dd27
layeeqahmed-dev-glitch <layeeqahmed.dev@gmail.com>
2026-07-02 21:11:06 +0530

    fixed: form and workflow creation

--- index.js
+++ index.js
@@ -1147,7 +1147,7 @@ app.post('/deal-webhook', async (req, res) => {
 
             hs_meeting_title: dealName,
             hs_meeting_body: `<b>${ownerName} is inviting you to a scheduled meeting.</b><br>
-            <b>Topic:</b> ${meeting_name}<br>
+            <b>Topic:</b> ${dealName}<br>
             <b>Date & Time:</b> ${meeting_date}, ${formattedTime}<br><br>
             <b>Join MeetHour:</b> ${meeting.joinURL}<br><br>
             <b>Meeting ID:</b> ${meeting.meeting_id}<br>
