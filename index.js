075fb36fb49d6e9367466fa85edf985b38f1a1de
layeeqahmed-dev-glitch <layeeqahmed.dev@gmail.com>
2026-07-08 14:28:54 +0530

    fixed: meeting details

--- index.js
+++ index.js
@@ -2267,11 +2267,11 @@ app.post("/create-meeting", async (req, res) => {
 
     const details = `
     <br><br>
-      <b>${ownerName} is inviting you to a scheduled meeting.</b><br>
-      <b>Topic:</b> ${meeting.topic}<br>
-      <b>Time:</b> ${formattedTime} (${resolvedTimezone})<br><br>
-      <b>Join MeetHour Meeting</b>: ${meeting.joinURL}<br><br>
-      <b>Meeting ID:</b> ${meeting.meeting_id}<br>
+      <b>${ownerName} is inviting you to a scheduled meeting.
+      <b>Topic:</b> ${meeting.topic}
+      <b>Time:</b> ${formattedTime} (${resolvedTimezone})
+      <b>Join MeetHour Meeting</b>: ${meeting.joinURL}
+      <b>Meeting ID:</b> ${meeting.meeting_id}
       <b>Passcode:</b> ${meeting.passcode}
     `;
 
